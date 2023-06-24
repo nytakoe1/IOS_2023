6668fd8e5a231f6ddddda1f9ce8d9298ac87548b
nytakoe1 <yana.pavlova.pole@gmail.com>
2023-06-24 16:36:18 +0300

    Initial Commit

---
+++ homeWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork3
+//
+//  Created by Яна Павлова on 24.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
